7c35dc46fe95a75ae33c28df1e885ffe11ef0f47
ShahzaibSE <shahzaibanalyst@outlook.com>
2020-08-08 00:40:47 +0500

    Added animation to services.

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -6,21 +6,33 @@ import useWebAnimations, {fadeIn,fadeInLeft} from "@wellyshen/use-web-animations
 // Assets.
 import "./Services.scss"
 import "animate.css/animate.min.css" // Scroller dependency.
+import service_landing_animation from "./../../images/services-landing-animation.gif"
+import {services_animations} from "./Services.style"
 
 const ServicesComponent = () => {
     const {ref} = useWebAnimations({...fadeIn, timing:{duration: 4000}})
     return (
         <Grid container>
-            <Grid item xs={12}>
+            <Grid item xs={6}>
                 <div ref={ref} className="service_page_heading_container">
                     <StyleRoot>
-                            <h2 className="service_page_heading">
+                            <h2 className="service_page_heading" 
+                                style={services_animations.service_landing_heading_animation}>
                                 <span>Impactful,<br/></span>
                                 <span>Innovative and,<br/></span>
                                 <span>Insightful<br/></span>
                             </h2>    
                     </StyleRoot>
                 </div>
+            </Grid>
+            <Grid item xs={6}>
+            <StyleRoot>  
+                    <div className="telling_about_services">
+                            <div>
+                                <img src={service_landing_animation} alt="Services we provide"/>
+                            </div>
+                    </div>
+                </StyleRoot> 
             </Grid>
         </Grid>
     )
--- src/components/Services/Services.style.js
+++ src/components/Services/Services.style.js
@@ -2,3 +2,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import { bounceIn, shake, pulse, fadeIn } from 'react-animations';
 import Radium from 'radium';
 import { red, green,lightBlue, cyan, amber, yellow, blueGrey, grey} from '@material-ui/core/colors';
+
+export const services_animations = {
+    service_landing_heading_animation: {
+        animation:"landing_heading_animation 2s infinite",
+        animationName: Radium.keyframes(pulse, 'pulse')
+      } 
+}
