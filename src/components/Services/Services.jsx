25e764e1b2c4adea37dc903bab39995bf1e86f7c
ShahzaibSE <shahzaibanalyst@outlook.com>
2020-08-08 23:43:56 +0500

    Adding services details.

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -3,14 +3,35 @@ import {StyleRoot} from 'radium'
 import {Grid} from "@material-ui/core"
 import {ScrollAnimation} from "react-animate-on-scroll"
 import useWebAnimations, {fadeIn,fadeInLeft} from "@wellyshen/use-web-animations"
+// For Services List Details.
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
 // Assets.
 import "./Services.scss"
 import "animate.css/animate.min.css" // Scroller dependency.
 import service_landing_animation from "./../../images/services-landing-animation.gif"
 import {services_animations} from "./Services.style"
+import {serviceUseStyles} from "./Services.style";
 
 const ServicesComponent = () => {
     const {ref} = useWebAnimations({...fadeIn, timing:{duration: 4000}})
+    const classes = serviceUseStyles();
+    const [open, setOpen] = React.useState(true);
+    // Collapser.
+    const handleClick = () => {
+        setOpen(!open);
+    };
+    //
     return (
         <Grid container>
             <Grid item xs={6}>
@@ -33,6 +54,48 @@ const ServicesComponent = () => {
                             </div>
                     </div>
                 </StyleRoot> 
+            </Grid>
+            <Grid item xs={12}>
+                        <List
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+                subheader={
+                    <ListSubheader component="div" id="nested-list-subheader">
+                    Nested List Items
+                    </ListSubheader>
+                }
+                className={classes.root}
+                >
+                <ListItem button>
+                    <ListItemIcon>
+                    <SendIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Sent mail" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                    <DraftsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Drafts" />
+                </ListItem>
+                <ListItem button onClick={handleClick}>
+                    <ListItemIcon>
+                    <InboxIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Inbox" />
+                    {open ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                    <ListItem button className={classes.nested}>
+                        <ListItemIcon>
+                        <StarBorder />
+                        </ListItemIcon>
+                        <ListItemText primary="Starred" />
+                    </ListItem>
+                    </List>
+                </Collapse>
+                </List>
             </Grid>
         </Grid>
     )
--- src/components/Services/Services.style.js
+++ src/components/Services/Services.style.js
@@ -3,6 +3,17 @@ import { bounceIn, shake, pulse, fadeIn } from 'react-animations';
 import Radium from 'radium';
 import { red, green,lightBlue, cyan, amber, yellow, blueGrey, grey} from '@material-ui/core/colors';
 
+export const serviceUseStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
 export const services_animations = {
     service_landing_heading_animation: {
         animation:"landing_heading_animation 2s infinite",
